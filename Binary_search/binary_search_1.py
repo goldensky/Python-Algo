29583e54bce55225281ddac23911aa73f48649a1
goldensky <tanya.generalova@gmail.com>
2023-05-20 14:27:26 +0300

    Adding to Binary_search folder

---
+++ Binary_search/binary_search_1.py
@@ -0,0 +1,65 @@
+import unittest
+
+
+def binary_search(lst, key):
+    low = 0
+    high = len(lst) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if lst[mid] == key:
+
+            return mid
+        elif lst[mid] > key:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+
+class TestBinarySearch(unittest.TestCase):
+    def test_binary_search_1(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = 5
+        self.assertEqual(binary_search(lst, key), 4)
+
+    def test_binary_search_2(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = 7
+        self.assertEqual(binary_search(lst, key), 5)
+
+    def test_binary_search_3(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = -3
+        self.assertEqual(binary_search(lst, key), -1)
+
+    def test_binary_search_4(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = 80
+        self.assertEqual(binary_search(lst, key), -1)
+
+    def test_binary_search_5(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = 20
+        self.assertEqual(binary_search(lst, key), -1)
+
+    def test_binary_search_6(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = -1
+        self.assertEqual(binary_search(lst, key), 0)
+
+    def test_binary_search_7(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = 28
+        self.assertEqual(binary_search(lst, key), 8)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Binary_search/sum_to_k.py
@@ -0,0 +1,58 @@
+import unittest
+
+
+def binary_search(lst, key):
+    low = 0
+    high = len(lst) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if lst[mid] == key:
+
+            return mid
+        elif lst[mid] > key:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+
+def sum_to_k(lst, k):
+    lst.sort()
+    total_result = []
+    for index, item in enumerate(lst):
+        index_y = binary_search(lst, k - item)
+        if index_y > -1:
+            total_result.append((item, lst[index_y]))
+
+    return total_result
+
+
+class TestSumToK(unittest.TestCase):
+    def test_sum_to_k_1(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = 12
+        estimated_result = [(0, 12), (5, 7), (7, 5), (12, 0)]
+
+        self.assertEqual(sum_to_k(lst, key), estimated_result)
+
+    def test_sum_to_k_2(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = 15
+        estimated_result = [(3, 12), (12, 3)]
+
+        self.assertEqual(sum_to_k(lst, key), estimated_result)
+
+    def test_sum_to_k_3(self):
+        lst = [12, 5, 28, 23, 4, 3, 7, -1, 0]
+        lst.sort()
+        key = 90
+        estimated_result = []
+
+        self.assertEqual(sum_to_k(lst, key), estimated_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
